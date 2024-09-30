62501d77ddba13aaa1295490b1b041570b72ebe9
Dennis Kibet <dennisrkibet@gmail.com>
2024-09-30 12:32:11 +0300

    passed the unit tests

--- server/app.py
+++ server/app.py
@@ -4,5 +4,42 @@ from flask import Flask
 
 app = Flask(__name__)
 
-if __name__ == '__main__':
+
+@app.route("/")
+def index():
+    return "<h1>Python Operations with Flask Routing and Views</h1>"
+
+
+@app.route("/print/<string:parameter>")
+def print_string(parameter):
+    return f"{parameter}"
+
+
+@app.route("/count/<int:parameter>")
+def count(parameter):
+    numbers = "\n".join(str(i) for i in range(parameter))
+    return f"{numbers}\n"
+
+
+@app.route("/math/<int:num1>/<string:operation>/<int:num2>")
+def math(num1, operation, num2):
+    if operation == "+":
+        result = num1 + num2
+    elif operation == "-":
+        result = num1 - num2
+    elif operation == "*":
+        result = num1 * num2
+    elif operation == "div":
+        if num2 == 0:
+            return "<p>Division by zero is not allowed.</p>"
+        result = num1 / num2
+    elif operation == "%":
+        result = num1 % num2
+    else:
+        return "<p>Invalid operation. Please use one of +, -, *, div, or %.</p>"
+
+    return f"{result}"
+
+
+if __name__ == "__main__":
     app.run(port=5555, debug=True)
